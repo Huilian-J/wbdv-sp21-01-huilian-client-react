8bc0e4762f2e386b4fceb6217fda9e1ed1ded7ba
Huilian-J <jiang.hui@northeastern.edu>
2021-02-22 15:27:15 -0500

    add course in course table, set courses in course manager

--- src/components/course-manager.js
+++ src/components/course-manager.js
@@ -3,12 +3,29 @@ import CourseTable from "./course-table";
 import CourseGrid from "./course-grid";
 import CourseEditor from "./course-editor";
 
-const CourseManager = () =>
-    <div>
-        <h1>Course Manager</h1>
-        <CourseTable/>
-        <CourseGrid/>
-        <CourseEditor/>
-    </div>
+class CourseManager
+    extends React.Component {
+
+    state = {
+        courses: [
+            {title:"CS1234", owner:"alice", lastModified:"1/1/21"},
+            {title:"CS2341", owner:"bob", lastModified:"2/2/21"},
+            {title:"CS3412", owner:"charlie", lastModified:"3/3/21"},
+            {title:"CS4123", owner:"dan", lastModified:"4/4/21"}
+
+        ]
+    }
+
+    render() {
+        return(
+            <div>
+                <h1>Course Manager</h1>
+                <CourseTable courses={this.state.courses}/>
+                <CourseGrid courses={this.state.courses}/>
+                <CourseEditor/>
+            </div>
+        )
+    }
+}
 
 export default CourseManager
--- src/components/course-table.js
+++ src/components/course-table.js
@@ -3,14 +3,36 @@ import CourseRow from "./course-row";
 
 export default class CourseTable
     extends React.Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    addCourse = () => {
+        const newCourse = {
+            title: "New Course",
+            owner: "New Owner",
+            lastModified: "Never"
+        }
+        this.props.courses.push(newCourse)
+        this.setState(this.state)
+    }
+
     render() {
         return(
             <div>
                 <h2>Course Table</h2>
+                <button onClick={this.addCourse}>Add Course</button>
                 <table className="table">
                     <tbody>
-                    <CourseRow title="CS1234" owner="alice" lastModified="1/1/20"/>
-                    <CourseRow title="CS2345" owner="bob" lastModified="1/1/21"/>
+                    {
+                        this.props.courses.map((course, ndx) =>
+                            <CourseRow
+                                key={ndx}
+                                title={course.title}
+                                owner={course.owner}
+                                lastModified={course.lastModified}/>)
+                    }
                     </tbody>
                 </table>
             </div>

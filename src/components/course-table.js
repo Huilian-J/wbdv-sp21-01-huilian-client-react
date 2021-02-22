962e968e08083ffd1c38b9dc3b98be08a84f9e23
Huilian-J <jiang.hui@northeastern.edu>
2021-02-22 14:45:04 -0500

    Created course row file, update course table

--- src/components/course-table.js
+++ src/components/course-table.js
@@ -1,10 +1,19 @@
 import React from 'react'
+import CourseRow from "./course-row";
 
 export default class CourseTable
     extends React.Component {
     render() {
         return(
-            <h2>Course Table</h2>
+            <div>
+                <h2>Course Table</h2>
+                <table className="table">
+                    <tbody>
+                    <CourseRow title="CS1234" owner="alice" lastModified="1/1/20"/>
+                    <CourseRow title="CS2345" owner="bob" lastModified="1/1/21"/>
+                    </tbody>
+                </table>
+            </div>
         )
     }
 }

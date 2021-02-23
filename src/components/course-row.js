6424bc02cf9a241e0d3ffe71423fc836b8cb71b8
Huilian-J <jiang.hui@northeastern.edu>
2021-02-23 13:57:11 -0500

    course-row: switch between edit and not edit status

--- src/components/course-row.js
+++ src/components/course-row.js
@@ -1,20 +1,35 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {Link} from "react-router-dom";
 
-const CourseRow = ({deleteCourse, course}) =>
-    <tr>
-        <td>
-            <Link to="/courses/editor">
-                {course.title}
-            </Link>
-        </td>
-        <td>{course.owner}</td>
-        <td>{course.lastModified}</td>
-        <td>
-            <i className="fas fa-check"></i>
-            <i onClick={() => deleteCourse(course)} className="fas fa-trash"></i>
-            <i className="fas fa-edit"></i>
-        </td>
-    </tr>
+const CourseRow = ({deleteCourse, course}) => {
+
+    const [editing, setEditing] = useState(false)
+
+    return (
+        <tr>
+            <td>
+                {
+                    !editing &&
+                    <Link to="/courses/editor">
+                        {course.title}
+                    </Link>
+                }
+                {
+                    editing &&
+                    <input value={course.title}
+                           className="form-control"/>
+                }
+            </td>
+            <td>{course.owner}</td>
+            <td>{course.lastModified}</td>
+            <td>
+                <i onClick={() => deleteCourse(course)} className="fas fa-trash"></i>
+                {!editing && <i onClick={() => setEditing(true)} className="fas fa-edit"></i>}
+                {editing && <i onClick={() => setEditing(false)} className="fas fa-check"></i>}
+            </td>
+        </tr>
+
+    )
+}
 
 export default CourseRow

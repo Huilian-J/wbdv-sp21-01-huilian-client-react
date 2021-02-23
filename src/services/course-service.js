49960d36bc52834abe42c31293325948cc87985a
Huilian-J <jiang.hui@northeastern.edu>
2021-02-23 12:45:36 -0500

    fix delete course

--- src/services/course-service.js
+++ src/services/course-service.js
@@ -1,5 +1,4 @@
 const COURSES_URL = "https://wbdv-generic-server.herokuapp.com/api/huilian/courses"
-
 export const findAllCourses = () =>
     fetch(COURSES_URL)
         .then(response => response.json())
@@ -12,7 +11,7 @@ export const createCourse = (course) =>
             }})
         .then(response => response.json())
 export const deleteCourse = (courseId) =>
-    fetch(`${COURSES_URL}/{courseId}`,
+    fetch(`${COURSES_URL}/${courseId}`,
         {method: 'DELETE'})
         .then(response => response.json())
 export const updateCourse = (courseId, course) => {}

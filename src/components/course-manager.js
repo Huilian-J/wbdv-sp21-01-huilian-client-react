528321345bbeb4f691ccb4da8ec5d03809e2506b
Huilian-J <jiang.hui@northeastern.edu>
2021-02-24 22:04:48 -0500

    added directory course-editor

--- src/components/course-manager.js
+++ src/components/course-manager.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import CourseTable from "./course-table/course-table";
 import CourseGrid from "./course-grid/course-grid";
-import CourseEditor from "./course-editor";
+import CourseEditor from "./course-editor/course-editor";
 import {Route} from "react-router-dom";
 import CourseService from "../services/course-service";
 

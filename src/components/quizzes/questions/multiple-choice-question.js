4feac8d948602ad124c46ab7abbee95c497f6973
Huilian-J <jiang.hui@northeastern.edu>
2021-04-12 23:03:57 -0400

    question update

--- src/components/quizzes/questions/multiple-choice-question.js
+++ src/components/quizzes/questions/multiple-choice-question.js
@@ -3,7 +3,24 @@ import React from 'react'
 const MultipleChoiceQuestion = ({question}) => {
     return(
         <div>
-            <h4>Multiple Choice Question</h4>
+            <h4>{question.question}</h4>
+            <ul className="list-group">
+                {
+                    question.choices.map((choice) => {
+                        return(
+                            <li className="list-group-item">
+                                <label>
+                                    <input type="radio" name={question._id} className="mr-1"/>
+                                    {choice}
+                                </label>
+                            </li>
+                        )
+
+                    })
+                }
+            </ul>
+            <h6 className="mt-2">Your answer: {question.correct}</h6>
+            <button className="btn btn-success mb-3">Grade</button>
         </div>
     )
 }
--- src/components/quizzes/questions/true-false-question.js
+++ src/components/quizzes/questions/true-false-question.js
@@ -3,7 +3,23 @@ import React from 'react'
 const TrueFalseQuestion = ({question}) => {
     return(
         <div>
-            <h4>True-False Question</h4>
+            <h4>{question.question}</h4>
+            <ul className="list-group">
+                <li className="list-group-item">
+                    <label>
+                        <input type="radio" name={question._id} className="mr-1"/>
+                        True
+                    </label>
+                </li>
+                <li className="list-group-item">
+                    <label>
+                        <input type="radio" name={question._id} className="mr-1"/>
+                        False
+                    </label>
+                </li>
+            </ul>
+            <h6 className="mt-2">Your answer: {question.correct}</h6>
+            <button className="btn btn-success mb-3">Grade</button>
         </div>
     )
 }
